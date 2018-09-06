ea5178e133246d8af426c9b8ffa2d66faa0137d6
neighbor983 <kenneth.ray.baker@gmail.com>
2018-09-06 18:04:03 +0000

    created a simple function to return theta for problem two

--- problem_two.py
+++ problem_two.py
@@ -9,3 +9,26 @@ its only minimum value of zero if θtXi  > 0 for all Xi.
 Develop a machine learning algorithm based on gradient descent approach and the 
 above cost function to learn θ from training data to separate the two classes. 
 '''
+import numpy as np;
+
+def algorithm(training_patterns, α):
+    '''
+    description:
+        Returns a theta if the the training_patterns provided are linearly 
+        separable by the two classes.
+    inputs:
+        training_patterns = np.matrix
+        α = number
+    output:
+        vector
+    '''
+
+    theta = np.ones(len(training_patterns[0]));
+    changeFlag = True;
+    while(changeFlag):
+        changeFlag = False;
+        for data in training_patterns:
+            if(theta.T * data < 0):
+                theta = theta + α * data;
+                changeFlag = True;
+    return theta;        
